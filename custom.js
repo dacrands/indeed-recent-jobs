3b90fca35282d89d5308f16fbedafb1fe5e45488
da crands <davecrands@gmail.com>
2020-04-18 19:59:34 -0400

    Change style of footer parent instead of footer

--- custom.js
+++ custom.js
@@ -2,6 +2,6 @@ let jobFooters = document.getElementsByClassName('jobsearch-SerpJobCard-footer')
 
 for (let footer of jobFooters) {
   if(!footer.innerText.includes('30+')) {
-    footer.style.backgroundColor = '#c5decc';
+    footer.parentElement.style.border = '2px solid #c5decc';
   }
 }
